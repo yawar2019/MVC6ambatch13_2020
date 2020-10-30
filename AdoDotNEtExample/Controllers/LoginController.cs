a0d490a80b4f09a31a13a23c1c939a0b2d17d6e1
yawar2019 <yawarali17@gmail.com>
2020-10-30 07:03:44 +0530

    checkin code

---
+++ AdoDotNEtExample/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdoDotNEtExample.Models;
+using System.Web.Security;
+
+namespace AdoDotNEtExample.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(UserModel uobj)
+        {
+            if (uobj.UserName == "Divya" && uobj.Password == "Divya")
+            {
+                FormsAuthentication.SetAuthCookie(uobj.UserName, false);
+                return Redirect("~/Login/Index");
+            }
+            else
+            {
+                ViewBag.msg = "Invalid UserName or Password";
+                  return View();
+            }
+
+           
+        }
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("~/Login/Login");
+        }
+    }
+}

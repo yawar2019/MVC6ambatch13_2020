40d5af8bb9dc780725a70b59f2f061ed86fb7b9b
yawar2019 <yawarali17@gmail.com>
2020-11-17 07:00:50 +0530

    Updated code

---
+++ ConsumingWebSerice/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumingWebSerice.ServiceReference1;
+namespace ConsumingWebSerice.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            MyCaclculatorSeviceSoapClient obj = new MyCaclculatorSeviceSoapClient();
+            obj.Add(12, 16);
+
+            return Content(obj.Add(12, 16).ToString());
+        }
+    }
+}

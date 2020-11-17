5d2cc4a0235813a1c6ad67c5b7692298a903cb32
yawar2019 <yawarali17@gmail.com>
2020-11-17 06:22:51 +0530

    Code Committed

---
+++ DBwithStoreproc/Controllers/DefaultController.cs
@@ -0,0 +1,63 @@
+﻿using DBwithStoreproc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBwithStoreproc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MvcDBEntities db1 = new MvcDBEntities();
+        public ActionResult Index()
+        {
+            return View(db1.Sp_GetEmployee());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Sp_GetEmployee_Result obj)
+        {
+            System.Data.Entity.Core.Objects.ObjectResult i = db1.sp_insertEmployeeExample2(obj.EmpName, obj.EmpSalary, obj.EmpDesignation);
+            int r = Convert.ToInt32(i);
+            if (r > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+        public ActionResult HtmlExamplle()
+        {
+            Sp_GetEmployee_Result db = new Sp_GetEmployee_Result();
+            db.EmpName = "under taker";
+            //ViewBag.Employees = new SelectList(db1.Sp_GetEmployee(), "Empid", "EmpName",4);
+            ViewBag.Employees12 = new SelectList(db1.employeeDetails, "Empid", "EmpName", 4);
+
+            return View(db);
+
+        }
+        public ActionResult ValidationExample()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ValidationExample(RegisterModel reg)
+        {
+            if (ModelState.IsValid) { return View(); }
+            else
+            {
+                return View(reg);
+
+            }
+        }
+    }
+}
---
+++ DBwithStoreproc/Models/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DBwithStoreproc.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage ="UserName is Required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        public string Pwd { get; set; }
+        [Compare("Pwd",ErrorMessage ="Pwd and CPwd are Not Matching")]
+        public string ConfirmPwd { get; set; }
+        [Range(12,50,ErrorMessage ="12-50 are Allowed")]
+        [Required(ErrorMessage = "Password is Required")]
+        public int age { get; set; }
+        //[DataType(DataType.EmailAddress,ErrorMessage ="Invalid EmailId")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage ="Emailid is Invalid")]
+        public string EmailId { get; set; }
+        [StringLength(10,ErrorMessage ="Max 10 Character")]
+        public string Address { get; set; }
+
+    }
+}

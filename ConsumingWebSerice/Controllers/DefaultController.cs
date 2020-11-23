1b1c8ff6c3d9b011b9c5455db1284dc7d2f2e8d9
yawar2019 <yawarali17@gmail.com>
2020-11-23 06:54:40 +0530

    Add Api code committed

--- ConsumingWebSerice/Controllers/DefaultController.cs
+++ ConsumingWebSerice/Controllers/DefaultController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ConsumingWebSerice.ServiceReference1;
+using ConsumingWebSerice.ServiceReference2;
 namespace ConsumingWebSerice.Controllers
 {
     public class DefaultController : Controller
@@ -14,7 +15,25 @@ namespace ConsumingWebSerice.Controllers
             MyCaclculatorSeviceSoapClient obj = new MyCaclculatorSeviceSoapClient();
             obj.Add(12, 16);
 
-            return Content(obj.Add(12, 16).ToString());
+            ServiceReference2.Service1Client obj1 = new ServiceReference2.Service1Client();
+            obj1.Add(25, 25);
+
+            return Content(obj1.Add(25, 25).ToString());
+        }
+        public ActionResult SendMethod() {
+
+            TempData["Student"] = "Malakonda";
+            return RedirectToAction("ReceiveMethod");
+        }
+        public ActionResult ReceiveMethod()
+        {
+
+            //string stdentName=Convert.ToString(TempData["Student"]);
+            //TempData.Keep();
+            string stdentName=TempData.Peek("Student").ToString();
+      
+
+            return Content(stdentName);
         }
     }
 }
